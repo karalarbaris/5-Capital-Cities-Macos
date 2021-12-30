d1c13141c7b7376332716c17182208bbf5f6cc55
Baris <karalarbaris@gmail.com>
2021-12-30 16:44:59 +0300

    Initial Commit

---
+++ 5-Capital-Cities-Macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  5-Capital-Cities-Macos
+//
+//  Created by Baris Karalar on 30.12.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
