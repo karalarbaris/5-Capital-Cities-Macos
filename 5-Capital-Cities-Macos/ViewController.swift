02e7560fd415b782759124d973b4aec4d3da18ac
Baris <karalarbaris@gmail.com>
2022-01-02 20:17:29 +0300

    Creating a map view

--- 5-Capital-Cities-Macos/ViewController.swift
+++ 5-Capital-Cities-Macos/ViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import Cocoa
-
+import MapKit
 class ViewController: NSViewController {
 
+    @IBOutlet var questionLabel: NSTextField!
+    @IBOutlet var scoreLabel: NSTextField!
+    @IBOutlet var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
